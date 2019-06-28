891f2853385512139cb5e10794f16fa9d3f6b606
cassieliu <cassieliu@augmentum.com.cn>
2019-06-28 10:05:14 +0800

    vue移动端开

--- frontend/vuejs/node_modules/vue/my-project/src/main.js
+++ frontend/vuejs/node_modules/vue/my-project/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
 
